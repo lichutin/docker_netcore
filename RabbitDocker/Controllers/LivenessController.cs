56c32c8891f79062833c096bff68ed01c789da61
Yurii Lichutin <peoplevs1@gmail.com>
2020-02-10 12:44:48 +0300

    Adds liveness and readness controllers

---
+++ RabbitDocker/Controllers/LivenessController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitDocker.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class LivenessController : ControllerBase
+    {
+        private static bool _osOk = true;
+
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return _osOk
+                ? new StatusCodeResult(200)
+                : new StatusCodeResult(500);
+        }
+
+        [HttpGet]
+        public IActionResult Set(bool isOk)
+        {
+            _osOk = isOk;
+            return new StatusCodeResult(200);
+        }
+    }
+}
