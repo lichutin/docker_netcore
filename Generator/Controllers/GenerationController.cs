4a5c3299c85975b24390cb4fc16959552aa46e58
Yurii Lichutin <peoplevs1@gmail.com>
2020-02-10 12:44:28 +0300

    Add second app for communication testing

---
+++ Generator/Controllers/GenerationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Generator.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class GenerationController : ControllerBase
+    {
+        private const string Alpabete = "АБВГДЕЁЖЗИЙКЛМНОРПСТABCDEFGHIJKLMNOPQR";
+
+        [HttpGet]
+        public string GetString()
+        {
+            var rnd = new Random();
+            
+            var res = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                var letterIndex = rnd.Next(0, Alpabete.Length);
+                res += Alpabete[letterIndex];
+            }
+
+            return res;
+        }
+
+        [HttpGet]
+        public int GetNumber()
+        {
+            return new Random().Next(0, 100);
+        }
+    }
+}
---
+++ RabbitDocker/Controllers/SpeakingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+
+namespace RabbitDocker.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SpeakingController : ControllerBase
+    {
+        private const string EnvVar = "REMOTE_ENDPOINT";
+
+        [HttpGet]
+        public string Check()
+        {
+            var variableValue = Environment.GetEnvironmentVariable(EnvVar);
+            return variableValue ?? "";
+        }
+
+        [HttpGet]
+        public async System.Threading.Tasks.Task<string> GetAsync()
+        {
+            var variableValue = Environment.GetEnvironmentVariable(EnvVar);
+
+            using var client = new HttpClient();
+
+            var numberUri = variableValue + "/generation/getnumber";
+            var number = await client.GetStringAsync(numberUri);
+
+            var stringUri = variableValue + "/generation/getstring";
+            var @string = await client.GetStringAsync(numberUri);
+
+            return @string + number;
+        }
+
+    }
+}
